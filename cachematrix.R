1a25700516f2e3b93ac6cd20eba686b018ff1a62
eraechoin <yfu1129@gmail.com>
2015-05-23 21:18:03 -0400

    New Commit CacheMatrix
    5/23 21:16

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## To eliminate repetitive calculations for matrix inverse, this code allows us to cache the inverse of a matrix
+## without repeated computations if the value already exists 
 
-## Write a short comment describing this function
+
+## This function creates a special "matrix" object
+## that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m<-NULL
+        set<-function(y){                               ##set the value of the matrix
+                x<<-y  
+                m<<-NULL
+        }
+        get <- function() {                             ##get the matrix
+                x
+        }
+        setinverse <- function(inverse){                ##set the inverse of the matrix
+                m <<- inverse
+        }
+        getinverse <- function(){                       ##get its inverse 
+                m      
+        } 
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special
+## "matrix" returned by `makeCacheMatrix` above. If the inverse has
+## already been calculated (and the matrix has not changed), then
+## `cacheSolve` should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()        
+        if(!is.null(m)) {                               ##if inverse exists, return it           
+                message("getting cached inverse")
+                return(m)                               ##we get the value and exit
+        }
+        data <- x$get()                                 ##otherwise compute it 
+        if(det(data)==0){                               ##test if determinent is zero                                
+                message("this matrix doesn't have an inverse")
+        }else{                                          #else we can compute its inverse
+                m <- solve(data, ...)
+                x$setinverse(m)
+                m
+        }
 }
